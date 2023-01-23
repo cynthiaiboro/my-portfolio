856d351bf5d996e2f172ef382702b5acd8d13e41
cynthiaiboro <cynthiaiboro@gmail.com>
2023-01-23 21:24:50 +0100

    fix deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  
+
+  ssr: false,
+
   target: 'static',
 
   // Global CSS: https://go.nuxtjs.dev/config-css
