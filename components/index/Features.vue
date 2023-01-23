9d4901f3da78465bdeac8daecf63f59898748cfc
cynthiaiboro <cynthiaiboro@gmail.com>
2023-01-23 21:17:38 +0100

    fix deployment

--- components/index/Features.vue
+++ components/index/Features.vue
@@ -2,22 +2,22 @@
   <div class="max-w-7xl mx-auto py-10">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:space-x-8">
         <div class="text-center bg-white rounded-lg place-self-center py-12 px-6">
-            <img class="h-8 mx-auto" src="../../assets/images/web.svg" alt="">
+            <img class="h-8 mx-auto" src="../../assets/images/web-img.svg" alt="">
             <h4 class="text-darktitle pt-5 font-bold">Design</h4>
             <p class="text-darkbody text-sm pt-2">Translate UI designs to clean and reusable codes</p>
         </div>
         <div class="text-center bg-white rounded-lg place-self-center py-12 px-6">
-            <img class="h-8 mx-auto" src="../../assets/images/real-time.svg" alt="">
+            <img class="h-8 mx-auto" src="../../assets/images/real-time-img.svg" alt="">
             <h4 class="text-darktitle pt-5 font-bold">Responsive view</h4>
             <p class="text-darkbody text-sm pt-2">Renders well on various devices and screen sizes</p>
         </div>
          <div class="text-center bg-white rounded-lg place-self-center py-12 px-6">
-            <img class="h-8 mx-auto" src="../../assets/images/Microservice.svg" alt="">
+            <img class="h-8 mx-auto" src="../../assets/images/microservice-img.svg" alt="">
             <h4 class="text-darktitle pt-5 font-bold">API Integration</h4>
             <p class="text-darkbody text-sm pt-2">Interaction with the database with the backend endpoints</p>
         </div>
          <div class="text-center bg-white rounded-lg place-self-center py-12 px-6">
-            <img class="h-8 mx-auto" src="../../assets/images/API.svg" alt="">
+            <img class="h-8 mx-auto" src="../../assets/images/api-img.svg" alt="">
             <h4 class="text-darktitle pt-5 font-bold">GIT Version Control</h4>
             <p class="text-darkbody text-sm pt-2">Collaborate with a team of engineers</p>
         </div>
